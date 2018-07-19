cd17bdc93ab84398fe13d546f76da1f6fd231b1c
GangstaRob <35857619+GangstaRob@users.noreply.github.com>
2018-07-19 13:40:26 -0500

    Update and rename talker.py to example.py

--- scripts/example.py
+++ scripts/example.py
@@ -6,11 +6,9 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher('chatter', String)
     rospy.init_node('talker')
-    while not rospy.is_shutdown():
-        str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(str)
-        pub.publish(String(str))
-        rospy.sleep(1.0)
+    str = "hello world %s" % rospy.get_time()
+    rospy.loginfo(str)
+    pub.publish(String(str))
 
 
 if __name__ == '__main__':
